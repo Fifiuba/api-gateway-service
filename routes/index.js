b5f2b3e3e068245b8f72bbfbed683590789a647f
solfonte <msfontenla@fi.uba.ar>
2022-09-14 15:45:21 -0300

    feat: added passenger creation redirection

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const http_status_code = require('http-status-codes')
 
 const usersServiceRouter = express.Router();
 
@@ -6,7 +7,13 @@ module.exports = {
     usersServiceRouter,
 }
 
-usersServiceRouter.route('/passengers/create')
-  .get(function (req, res, next) {
-    res.redirect('/') 
+/**
+ * @swagger
+ *
+ */
+
+usersServiceRouter.route('/passenger/create')
+  .post(async (req, res, next) => {
+    console.log('redirecting to: http://localhost:8000'+ req.originalUrl);
+    res.redirect(307,'http://localhost:8000'+ req.originalUrl); 
 })
