480312771742771a093ceeb55bf871f99e27d6f8
solfonte <msfontenla@fi.uba.ar>
2022-09-18 17:21:45 -0300

    refactor: removed swagger specifications

--- server.js
+++ server.js
@@ -3,32 +3,10 @@ const routes = require('./routes')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerSpec = {
-    definition : {
-        openapi: "3.0.0",
-        info: {
-            title: "API-Gateway",
-            version: "1.0.0",
-        },
-        servers: [
-            {
-                url: "http://localhost:8001"
-            }
-        ]
-    },
-    apis: [`${path.join(__dirname, "./routes/index.js")}`],  
-};
-
-
-const app = express()
-
-
+const app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(swaggerSpec)));
 app.use(routes.servicesRouter);
 
 app.get('/', function(req,res,next){
