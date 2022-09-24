ef1b7631dc20ee4ea544ebb6b15ea7a4f39b036e
solfonte <msfontenla@fi.uba.ar>
2022-09-24 19:38:38 -0300

    fix: fixedjwt expiration response

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,24 @@ const status = require('http-status')
 const axios = require('axios')
 const registry = require('./registry.json')
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+/*TODO: ver si tendria que hacer un axios.create()
+        arreglar el docker para que conecte con otras networks
+        tests
+        login ver si funciona bien
+*/
+const MockAdapter = require("axios-mock-adapter");
+let axiosInstance = axios.create()
+console.log('is ' + process.env.TEST_ENV == 'true')
+if (process.env.TEST_ENV == 'true'){
+  axiosInstance = new MockAdapter(axios)
+  console.log('entered')
+}
 
 const servicesRouter = express.Router();
 
-module.exports = {servicesRouter}
+module.exports = {servicesRouter, axiosInstance}
 
 async function authenticateToken (req,res,next){
   const authHeader = req.headers['authorization']
@@ -18,35 +32,34 @@ async function authenticateToken (req,res,next){
     jwt.verify(token,secretOrPublicKey="misupercontrasecreta")
     next()
   }catch (err){
-    const decoded = jwt.decode(token,secretOrPublicKey="misupercontrasecreta")
-    let login_url = registry.services['admins'] + '/admins/login'
-    if (decoded.user == true) login_url = registry.services['users'] + '/admins/login'
-    const response = await axios.get(login_url)
+    res.send({"status": 403, "detail": "expired token"})//TODO: estandarizar
+  }
+}
+
+async function redirectToService(req,res){
+  var url = registry.services[req.params.apiName].url + req.params.apiName + (req.params.path? '/' + req.params.path : '');
+  console.log(url)
+  try {
+    const response = await axiosInstance({
+      method: req.method,
+      url: url,
+      data: req.body
+    });
     res.send(response.data)
+
+  }catch (err){
+    res.send(err.code)
   }
 }
 
 servicesRouter.all("/:apiName/:path?", authenticateToken, (req, res) => {
+  print('aca entra')
   if (req.originalUrl.includes('favicon.ico')) {
     res.status(204).end()
 
   }else{
-    const redirectToService = async (req,res) => {
-      var url = registry.services[req.params.apiName].url + req.params.apiName + (req.params.path? '/' + req.params.path : '');
-      console.log(url)
-      try {
-        const response = await axios({
-          method: req.method,
-          url: url,
-          data: req.body
-        });
-        res.send(response.data);
+    redirectToService(req, res)
 
-      }catch (err){
-        res.send(err.code);
-      }
-    }
-    redirectToService(req, res);
   }
 });
 
