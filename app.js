6769931f657eb58d6bdc79b9711e1860e61eb20c
solfonte <msfontenla@fi.uba.ar>
2022-11-22 19:45:47 -0300

    feat: added swagger

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger.json');
 const cors = require('cors');
 
 const app = express();
@@ -8,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(routes.servicesRouter);
 
 app.get('/', function(req, res, next) {
